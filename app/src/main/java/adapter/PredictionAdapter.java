290e05956829d98021c51b27af6f8f8bee0ae6f4
Lukman <lnhafizramli@gmail.com>
2024-06-04 04:34:57 +0800

    finishing PredictionFragment

---
+++ app/src/main/java/adapter/PredictionAdapter.java
@@ -0,0 +1,216 @@
+package adapter;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ProgressBar;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import com.anychart.APIlib;
+import com.anychart.AnyChart;
+import com.anychart.AnyChartView;
+import com.anychart.chart.common.dataentry.DataEntry;
+import com.anychart.chart.common.dataentry.ValueDataEntry;
+import com.anychart.charts.Cartesian;
+import com.anychart.core.cartesian.series.Column;
+import com.anychart.enums.Anchor;
+import com.anychart.enums.HoverMode;
+import com.anychart.enums.Position;
+import com.anychart.enums.TooltipPositionMode;
+import com.anychart.enums.Layout;
+import com.anychart.graphics.vector.Stroke;
+import com.example.finalyearproject.R;
+
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
+
+import model.InfoPredict;
+
+public class PredictionAdapter extends BaseAdapter {
+
+    private final Context mContext;
+    private List<InfoPredict> mData;
+
+    public PredictionAdapter(Context mContext, List<InfoPredict> mData) {
+        this.mContext = mContext;
+        this.mData = mData;
+    }
+
+    @Override
+    public int getCount() {
+        return mData.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return mData.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @SuppressLint("SetTextI18n")
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        View v = convertView;
+
+        if (v == null) {
+            LayoutInflater inflater = LayoutInflater.from(mContext);
+            v = inflater.inflate(R.layout.prediction_cardview, parent, false);
+        }
+
+        if (convertView == null) {
+            TextView textDistrict = v.findViewById(R.id.textDistrict);
+            TextView textLocation = v.findViewById(R.id.textLocation);
+            TextView textAlert = v.findViewById(R.id.textAlert);
+            TextView textPrediction = v.findViewById(R.id.textPrediction);
+            TextView textWL = v.findViewById(R.id.textWL);
+            RelativeLayout alertLayout = v.findViewById(R.id.alertLayout);
+
+            // Setting up the text
+            InfoPredict infoPredict = mData.get(position);
+            textDistrict.setText(infoPredict.getDistrict());
+            textLocation.setText(infoPredict.getLocation());
+
+            // Check the highest predicted water level among the three days
+            double highestWaterLevel = Math.max(infoPredict.getDay1(), Math.max(infoPredict.getDay2(), infoPredict.getDay3()));
+            double floodLevel = highestWaterLevel - 3;
+            String floodLevelString = String.format("%.2f", floodLevel);
+            // Format the water level values to display only two decimal places
+            String formattedWaterLevel = String.format("%.2f", highestWaterLevel);
+
+            // Determine the index of the day with the highest water level
+            int highestWaterLevelIndex;
+            if (highestWaterLevel == infoPredict.getDay1()) {
+                highestWaterLevelIndex = 0;
+            } else if (highestWaterLevel == infoPredict.getDay2()) {
+                highestWaterLevelIndex = 1;
+            } else {
+                highestWaterLevelIndex = 2;
+            }
+
+            // Get the current day of the week
+            Calendar calendar = Calendar.getInstance();
+            int dayOfWeek = calendar.get(Calendar.DAY_OF_WEEK);
+
+            // Calculate the day when the flood is predicted
+            String[] daysOfWeek = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
+            String predictedDay = daysOfWeek[(dayOfWeek + highestWaterLevelIndex + 1) % 7]; // +1 to start from the next day
+
+            if (highestWaterLevel < 2.4) {
+                textAlert.setText("Status: Normal");
+                textPrediction.setText("No flood is predicted within 3 days");
+                textWL.setText("Highest: " + formattedWaterLevel + " Meter");
+                alertLayout.setBackgroundResource(R.color.lightgreen);
+            } else if (highestWaterLevel >= 2.4 && highestWaterLevel < 2.7) {
+                textAlert.setText("Status: Alert");
+                textPrediction.setText("No flood is predicted within 3 days");
+                textWL.setText("Highest: " + formattedWaterLevel + " Meter");
+                alertLayout.setBackgroundResource(R.color.lemonyellow);
+            } else if (highestWaterLevel >= 2.7 && highestWaterLevel <= 3) {
+                textAlert.setText("Status: Warning");
+                textPrediction.setText("No flood is predicted within 3 days");
+                textWL.setText("Highest: " + formattedWaterLevel + " Meter");
+                alertLayout.setBackgroundResource(R.color.orange);
+            } else if (highestWaterLevel > 3) {
+                textAlert.setText("Status: Danger");
+                textPrediction.setText("Flood is predicted on " + predictedDay);
+                textWL.setText("Flood Level: " + floodLevelString + " Meter");
+                alertLayout.setBackgroundResource(R.color.redpastel);
+            }
+
+            setupBarChart(v, infoPredict);
+        }
+
+        return v;
+    }
+
+    public void setupBarChart(View v, InfoPredict infoPredict){
+        // For the bar chart
+        AnyChartView anyChartView = v.findViewById(R.id.any_chart_view);
+        ProgressBar progressBar = v.findViewById(R.id.progress_bar);
+        anyChartView.setProgressBar(progressBar);
+        APIlib.getInstance().setActiveAnyChartView(anyChartView);
+
+        // Creating the chart
+        Cartesian cartesian = AnyChart.column();
+        cartesian.crosshair().yLabel(true).yStroke((Stroke) null, null, null, (String) null, (String) null);
+
+        // Get the current day of the week
+        Calendar calendar = Calendar.getInstance();
+        int dayOfWeek = calendar.get(Calendar.DAY_OF_WEEK);
+
+        // Create an array of day names
+        String[] daysOfWeek = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
+
+        // Calculate the next three days starting from the next day
+        String day1 = daysOfWeek[(dayOfWeek + 1) % 7];
+        String day2 = daysOfWeek[(dayOfWeek + 2) % 7];
+        String day3 = daysOfWeek[(dayOfWeek + 3) % 7];
+
+        // Format the data to display only two decimal places
+        String formattedDay1 = String.format("%.2f", infoPredict.getDay1());
+        String formattedDay2 = String.format("%.2f", infoPredict.getDay2());
+        String formattedDay3 = String.format("%.2f", infoPredict.getDay3());
+
+        List<DataEntry> data = new ArrayList<>();
+        data.add(new ValueDataEntry(day1, Double.parseDouble(formattedDay1)));
+        data.add(new ValueDataEntry(day2, Double.parseDouble(formattedDay2)));
+        data.add(new ValueDataEntry(day3, Double.parseDouble(formattedDay3)));
+
+        Column column = cartesian.column(data);
+
+        column.tooltip()
+                .titleFormat("{%X}")
+                .position(Position.CENTER_BOTTOM)
+                .anchor(Anchor.CENTER_BOTTOM)
+                .offsetX(0d)
+                .offsetY(5d)
+                .format("{%Value}{groupsSeparator: } Meter");
+
+        cartesian.animation(false);
+
+        cartesian.title("3 Days Water Level Prediction");
+
+        cartesian.yScale().minimum(0d);
+
+        cartesian.yAxis(0).labels().format("{%Value}{groupsSeparator: } Meter");
+
+        cartesian.tooltip().positionMode(TooltipPositionMode.POINT);
+        cartesian.interactivity().hoverMode(HoverMode.BY_X);
+
+        cartesian.xAxis(0).title("Day");
+        cartesian.yAxis(0).title("Water Level");
+
+        // Add line markers with different colors
+        cartesian.lineMarker(0)
+                .value(2.2)
+                .stroke("2 #00FF00")  // Green color for normal
+                .layout(Layout.HORIZONTAL);
+
+        cartesian.lineMarker(1)
+                .value(2.4)
+                .stroke("2 #FFFF00")  // Yellow color for alert
+                .layout(Layout.HORIZONTAL);
+
+        cartesian.lineMarker(2)
+                .value(2.7)
+                .stroke("2 #FFA500")  // Orange color for warning
+                .layout(Layout.HORIZONTAL);
+
+        cartesian.lineMarker(3)
+                .value(3)
+                .stroke("2 #FF0000")  // Red color for danger
+                .layout(Layout.HORIZONTAL);
+
+        anyChartView.setChart(cartesian);
+    }
+
+}
--- app/src/main/java/com/example/finalyearproject/PredictionFragment.java
+++ app/src/main/java/com/example/finalyearproject/PredictionFragment.java
@@ -2,19 +2,119 @@ package com.example.finalyearproject;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import adapter.PredictionAdapter;
+import model.InfoPredict;
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 
 public class PredictionFragment extends Fragment {
+    private ListView listPredInfo;
+    private PredictionAdapter adapter;
+    private List<InfoPredict> dataList;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_prediction, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        // Finding the view
+        listPredInfo = view.findViewById(R.id.listPredInfo);
+
+        dataList = new ArrayList<>();
+
+        // Create and set up the adapter
+        adapter = new PredictionAdapter(getContext(), dataList);
+        listPredInfo.setAdapter(adapter);
+
+        OkHttpClient okHttpClient = new OkHttpClient();
+
+        Request request = new Request.Builder().url("http://10.0.2.2:5000/get-predictions-by-today-date").header("Connection", "close").build();
+
+        // Make asynchronous call
+        okHttpClient.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(@NonNull Call call, @NonNull IOException e) {
+
+                String message = "Network Error";
+                Log.e("NetworkError", "10.0.2.2:5000/get-predictions-by-today-date", e);
+
+                if (e!=null){
+                    message = e.getMessage();
+                }
+                // Ensure Toast is shown on the main UI thread
+                final String finalMessage = message;
+                getActivity().runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        Toast.makeText(getActivity(), finalMessage, Toast.LENGTH_SHORT).show();
+                    }
+                });
+            }
+
+            @Override
+            public void onResponse(@NonNull Call call, @NonNull Response response) throws IOException {
+                try {
+                    JSONArray jsonArray = new JSONArray(response.body().string());
+                    for (int i = 0; i < jsonArray.length(); i++) {
+                        JSONObject jsonObject = jsonArray.getJSONObject(i);
+
+                        String district = jsonObject.getString("district");
+                        String location = jsonObject.getString("location");
+                        String date = jsonObject.getString("date");
+                        double testLoss = jsonObject.getDouble("testLoss");
+                        double MAE = jsonObject.getDouble("MAE");
+                        double MSE = jsonObject.getDouble("MSE");
+                        double RMSE = jsonObject.getDouble("RMSE");
+                        double day1 = jsonObject.getDouble("day1");
+                        double day2 = jsonObject.getDouble("day2");
+                        double day3 = jsonObject.getDouble("day3");
+
+
+                        // Create InfoPredict object and add to dataList
+                        InfoPredict data = new InfoPredict(date, district, location, testLoss, MAE, MSE, RMSE, day1, day2, day3);
+                        dataList.add(data);
+                    }
+
+                    // Notify the adapter of data changes
+                    getActivity().runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            adapter.notifyDataSetChanged();
+                        }
+                    });
+                } catch (JSONException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        });
     }
 }
