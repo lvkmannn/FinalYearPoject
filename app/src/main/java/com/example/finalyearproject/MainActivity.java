808249d4f37eac8fe4f9f1f5f0ea109450bfe1a6
Lukman <lnhafizramli@gmail.com>
2024-07-14 21:50:18 +0800

    Add code in MainActivity.java to send device token to DynamoDB

--- app/src/main/java/com/example/finalyearproject/MainActivity.java
+++ app/src/main/java/com/example/finalyearproject/MainActivity.java
@@ -7,10 +7,22 @@ import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MenuItem;
 import android.widget.FrameLayout;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.firebase.messaging.FirebaseMessaging;
+
+import java.io.IOException;
+
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -30,7 +42,7 @@ public class MainActivity extends AppCompatActivity {
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
 
                 int itemId = item.getItemId();
-                
+
                 if (itemId == R.id.navHome){
                     loadFragment(new HomeFragment(), false);
                 } else if (itemId == R.id.navOwnArea) {
@@ -48,6 +60,24 @@ public class MainActivity extends AppCompatActivity {
             }
         });
         loadFragment(new HomeFragment(), true);
+
+        // Retrieve and log the FCM token
+        FirebaseMessaging.getInstance().getToken().addOnCompleteListener(new OnCompleteListener<String>() {
+            @Override
+            public void onComplete(@NonNull Task<String> task) {
+                if (!task.isSuccessful()) {
+                    Log.w("MainActivity", "Fetching FCM registration token failed", task.getException());
+                    return;
+                }
+
+                // Get new FCM registration token
+                String token = task.getResult();
+                Log.d("FCM", "FCM Registration Token: " + token);
+
+                // Send token to your server or handle as needed
+                sendTokenToServer(token);
+            }
+        });
     }
 
     private void loadFragment(Fragment fragment, boolean isAppInitialized){
@@ -61,4 +91,37 @@ public class MainActivity extends AppCompatActivity {
         }
         fragmentTransaction.commit();
     }
+
+    private void sendTokenToServer(String token) {
+        OkHttpClient client = new OkHttpClient();
+
+        MediaType JSON = MediaType.get("application/json; charset=utf-8");
+        // Update JSON structure to match expected payload
+        String json = "{\"FCMToken\":\"" + token + "\"}";
+        Log.d("MainActivity", "JSON Payload: " + json); // Add this line to log the JSON payload
+
+        RequestBody body = RequestBody.create(json, JSON);
+        Request request = new Request.Builder()
+                .url("https://lvkmannn.pythonanywhere.com/add-device-token")
+                .post(body)
+                .build();
+
+        client.newCall(request).enqueue(new okhttp3.Callback() {
+            @Override
+            public void onFailure(@NonNull okhttp3.Call call, @NonNull IOException e) {
+                Log.e("MainActivity", "Failed to send token to server", e);
+            }
+
+            @Override
+            public void onResponse(@NonNull okhttp3.Call call, @NonNull Response response) throws IOException {
+                if (!response.isSuccessful()) {
+                    Log.e("MainActivity", "Unexpected code " + response);
+                    Log.e("MainActivity", "Response body: " + response.body().string());
+                } else {
+                    Log.d("MainActivity", "Token successfully sent to server");
+                }
+            }
+        });
+    }
+
 }
